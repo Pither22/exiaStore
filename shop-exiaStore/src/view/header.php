64ff99972ef810c985743dd3846ae4f8ad82e9d9
namir <anamir94@gmail.com>
2014-06-20 09:29:43 +0200

    coucou

--- shop-exiaStore/src/view/header.php
+++ shop-exiaStore/src/view/header.php
@@ -1,16 +1,41 @@
 <html lang="fr">
 <meta HTTP-EQUIV="Pragma" CONTENT="no-cache"> 
 <head>
-<meta charset="utf-8"/>
-<title>Shop ExiaStore</title>
+<title>eXia Store</title>
+    <!-- On ouvre la fenÃªtre Ã  la largeur de l'Ã©cran -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- IntÃ©gration du CSS Bootstrap -->
+    <link href="media/lib/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+    <!-- IntÃ©gration du CSS responsive Bootstrap -->
+    <link href="media/lib/bootstrap/css/bootstrap.css" rel="stylesheet">
 <meta content="" name="description"/>
 <meta content="" name="author"/>
 </head>
 <body>
-<div class="header">
-
-HEADER 
-<a href="index.php?page=accueil">Accueil</a>
-<a href="index.php?page=login">Connexion</a>
-
-</div>
+     <!-- Barre de navigation -->
+      <div class="navbar navbar-inverse">
+        <div class="navbar-inner">
+          <!-- Bouton apparaissant sur les rÃ©solutions mobiles afin de faire apparaÃ®tre le menu de navigation -->
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">eXia Store</a>
+          <!-- Structure du menu -->
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Sign Up</a></li>
+              <li><a href="#">Log In</a></li>
+              <li><a href="#">Shop</a></li>
+              
+            </ul>
+            <!-- Champ de recherche -->
+            <form class="navbar-search pull-right" action="#">
+              <input type="text" class="search-query span2" placeholder="Research">
+            </form>
+          </div>
+        </div>
+      </div>
+	  
